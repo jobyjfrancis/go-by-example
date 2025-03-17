3479002da5586afac501616687ccebbaed12bc7a
Joby Francis <joby.francis@silverstripe.com>
2025-03-17 17:20:31 +1300

    iterators

--- range-over-iterators/range-over-iterators.go
+++ range-over-iterators/range-over-iterators.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	"iterator"
+	"fmt"
+	"iter"
+	"slices"
 )
 
 type List[T any] struct {
 	head, tail *element[T]
 }
 
-type element[T any] struct {
+type element [T]struct {
 	next *element[T]
 	val  int
 }
 
-func (lst *List[T]) Push(v T) *List[T] {
+func (lst *List[T]) Push(v T) {
 	if lst.tail == nil {
 		lst.head = &element[T]{val: v}
 		lst.tail = lst.head
@@ -21,11 +23,30 @@ func (lst *List[T]) Push(v T) *List[T] {
 		lst.tail.next = &element[T]{val: v}
 		lst.tail = lst.tail.next
 	}
-	return lst
 }
 
-func (lst *List[T]) All() iterator.Seq[T] {
+func (lst *List[T]) All() iter.Seq[T] {
 	return func(yield func(T) bool) {
 
+		for e := lst.head; e != nil; e = e.next {
+			if !yield(e.val) {
+				return
+			}
+
+		}
 	}
+}
+
+func main() {
+	lst := List[int]{}
+	lst.Push(10)
+	lst.Push(20)
+	lst.Push(30)
+
+	for e := range lst.All() {
+		fmt.Println(e)
+	}
+
+	all := slices.Collect(lst.All())
+	fmt.Println("all: ", all)
 }

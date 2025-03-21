581afaf8a4b82e5d34bf1d4ce685bf1a6da90418
Joby Francis <joby.francis@silverstripe.com>
2025-03-21 17:31:04 +1300

    errors

--- errors/errors.go
+++ errors/errors.go
@@ -2,12 +2,48 @@ package main
 
 import (
 	"errors"
+	"fmt"
 )
 
-func f(arg int) (int, error) {
-	if arg == 42 {
+func f(i int) (int, error) {
+	if i == 42 {
 		return -1, errors.New("can't work with 42")
 	}
+	return i + 3, nil
+}
 
-	return arg + 3, nil
+var ErrOutOfTea = fmt.Errorf("no more tea available")
+var ErrPower = fmt.Errorf("can't boil water")
+
+func makeTea(i int) error {
+	if i == 2 {
+		return ErrOutOfTea
+	} else if i == 4 {
+		return fmt.Errorf("making tea: %w", ErrPower)
+	}
+	return nil
+}
+
+func main() {
+	for _, i := range []int{7, 42} {
+		if r, e := f(i); e != nil {
+			fmt.Println("f failed:", e)
+		} else {
+			fmt.Println("f worked:", r)
+		}
+	}
+
+	for i := range 5 {
+		if err := makeTea(i); err != nil {
+			if errors.Is(err, ErrOutOfTea) {
+				fmt.Println("We should buy new tea!")
+			} else if errors.Is(err, ErrPower) {
+				fmt.Println("Now it is dark")
+			} else {
+				fmt.Printf("unknown error: %s\n", err)
+			}
+			continue
+		}
+		fmt.Println("Tea is ready!")
+	}
 }
